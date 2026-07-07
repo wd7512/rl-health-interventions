46024505377907c27c7120576e192d5d9ff12c28
wd7512 <wwdennis.home@gmail.com>
2026-07-08 00:22:10 +0100

    updating request

--- src/rl_health_interventions/llm_bootstrapping/main.py
+++
@@ -1,7 +0,0 @@
-# import necessary libraries including tqdm for progress
-
-# Build messages
-
-# Call litellm
-
-# Save results to .jsonl file
---
+++ src/rl_health_interventions/llm_bootstrapping/request.py
@@ -0,0 +1,17 @@
+
+# import necessary libraries including tqdm for progress
+
+def perform_llm_bootstrapping():
+
+    # Build messages
+    # read config yaml file -> return a list of messages to send to the LLM
+
+    # Call litellm
+    # use concurrency similar to the example.py file to send messages to the LLM
+    # deafult 100
+
+
+    # Save results to .jsonl file
+    # to the correct folder
+
+    return None
